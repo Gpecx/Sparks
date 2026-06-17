df341d6c0e6b95a14ae5f53404502e399bc3676a
FredMotta00 <fredericocaires56@gmail.com>
2026-06-17 10:07:52 -0300

    fix(i18n tools): adiciona import de app_localizations no tool_kit.dart

--- lib/screens/tools/widgets/tool_kit.dart
+++ lib/screens/tools/widgets/tool_kit.dart
@@ -1,3 +1,4 @@
+import 'package:spark_app/l10n/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:spark_app/theme/app_theme.dart';
